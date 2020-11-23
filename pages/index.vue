6f8055cb58dec45f2c8b3336d6b43c5c9ee5324c
Muhammad Izzuddin Al Fikri <izzuddin.alfikri@icloud.com>
2020-11-23 20:34:15 +0700

    Remove dummy method

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,7 @@
 </template>
 
 <script>
+import ENV from '@/services/env'
 import Session from '@/mixins/session'
 import WelcomeIllustration from '@/components/landing-page/welcome-illustration'
 
@@ -18,19 +19,6 @@ export default {
     WelcomeIllustration,
   },
   mixins: [Session],
-  async asyncData({ $axios }) {
-    const options = {
-      headers: {
-        common: {
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6ImFjY2VzcyJ9.eyJpYXQiOjE2MDQyOTg4MjUsImV4cCI6MTYwNDM4NTIyNSwiYXVkIjoiaHR0cHM6Ly95b3VyZG9tYWluLmNvbSIsImlzcyI6ImZlYXRoZXJzIiwic3ViIjoiNWY5ZmE4NDI0MTE2NWU1OWY2MWM1YjUxIiwianRpIjoiNWQ5NGQ1NWUtNjBlMS00NTdjLWIxNWUtMTk0YmE5OWI1MjJhIn0.m18qo7Vp1wzWy02QrPTaWrp8HKQ9woapfuwS6WW3-BA',
-        },
-      },
-    }
-    const a = await $axios.$get('http://localhost:3030/peserta', options)
-
-    return { daftar_peserta: a.data }
-  },
   mounted() {
     // from mixins@Session
     this.setupCurrentSession()
