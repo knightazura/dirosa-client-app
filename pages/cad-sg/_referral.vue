d8fee084a3af8ca128d3cc3ae9130d0ad21c8cc7
Muhammad Izzuddin Al Fikri <ikhieasuka@gmail.com>
2020-12-08 17:03:35 +0700

    Add "show status after join" feature

---
+++ pages/cad-sg/_referral.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Refer success!</div>
+</template>
+
+<script>
+export default {
+  created() {
+    localStorage.setItem('referral_code', this.$route.params.referral)
+
+    this.$router.push('/')
+  }
+}
+</script>
+
+<style>
+
+</style>
