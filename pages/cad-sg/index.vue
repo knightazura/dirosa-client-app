cc2f61235332ff580a68fcd649640a89298aebca
Muhammad Izzuddin Al Fikri <izzuddin.alfikri@icloud.com>
2020-12-09 11:05:26 +0700

    Use query param instead for referral

--- pages/cad-sg/index.vue
+++ pages/cad-sg/index.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>Redirect...</div>
+  <div>Referral success! Redirect...</div>
 </template>
 
 <script>
 export default {
   created() {
+    if(process.browser)
+      localStorage.setItem('referral_code', this.$route.query.rc)
+
     this.$router.push('/');
   }
 }
